8113312b81fe69bff4d027f1b304857c97942ffe
Zach Carter <thezachcarter@gmail.com>
2022-04-01 15:35:56 -0500

    add files and folders for base components, basic set up and imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import {useState, useEFfect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+
+      
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
