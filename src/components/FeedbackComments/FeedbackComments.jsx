a2d64eed49c3e53bc301f7f357a58f069c2fcfc3
Zach Carter <thezachcarter@gmail.com>
2022-04-03 15:01:53 -0500

    center comments

--- src/components/FeedbackComments/FeedbackComments.jsx
+++ src/components/FeedbackComments/FeedbackComments.jsx
@@ -6,16 +6,18 @@ import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 //MUI
 const useStyles = makeStyles((theme) => ({
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
+        flexGrow: 1,
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
     },
-  }));
+}));
 
 function FeedbackComments() {
     const dispatch = useDispatch();
@@ -43,24 +45,26 @@ function FeedbackComments() {
             <h2>Any comments you want to leave?</h2>
 
             <form className={classes.root} noValidate autoComplete="off"
-            onSubmit={(event) => handleNext(event)}>
+                onSubmit={(event) => handleNext(event)}>
 
                 {/* INPUT BEFORE MUI
                 <input type='text' placeholder='Comments' onChange={handleComments} /> */}
-                
-                <TextField
-                id="outlined-multiline-static"
-                label="Comments?"
-                multiline
-                rows={4}
-                variant="outlined"
-                onChange={handleComments}
-                />
-                
-                <Button type='submit' variant='contained' color='primary'>NEXT</Button>
-                
-            </form>
 
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Comments?"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    onChange={handleComments}
+                />
+
+                <br></br>
+                {/* this break exists to center the form elements
+                had issues with MUI cooperating with other layout modifications */}
+
+                <Button type='submit' variant='contained' color='primary'>NEXT</Button>
+            </form>
         </div>
     )
 }
